feffa86cb00e55282395928c4fb43ef78541ad56
Jorge Romero <Jorgeddrv@gmail.com>
2021-07-17 11:47:40 -0400

    chore: cors config was modified

--- src/main/java/com/wecode/bookStore/configuration/CrossOriginConfig.java
+++ src/main/java/com/wecode/bookStore/configuration/CrossOriginConfig.java
@@ -3,12 +3,21 @@ package com.wecode.bookStore.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CrossOriginConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+            .addMapping("/**")
+            .allowedMethods(HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+            .allowedOrigins("*")
+            .allowCredentials(false)
+            .allowedHeaders("*")
+            .maxAge(3600);
     }
 }
